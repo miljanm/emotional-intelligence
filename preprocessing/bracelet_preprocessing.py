a3c9c8f363435f0c03f0965ba21a3d5bbf5546fd
mtthss <matteo.hessel@mail.polimi.it>
2015-03-18 12:37:41 +0000

    add preprocessing bracelet file

---
+++ preprocessing/bracelet_preprocessing.py
@@ -0,0 +1,33 @@
+
+
+time_slot = 30
+
+
+#################
+# Accelerometer #
+#################
+ACC_hertz = 32
+ACCsamples_slot = ACC_hertz*time_slot
+
+
+#########################
+# Blood volume pressure #
+#########################
+
+BVP_hertz = 64
+BVPsamples_slot = BVP_hertz*time_slot
+
+
+##########################
+# Electrodermal Activity #
+##########################
+
+EDA_hertz = 4
+EDAsamples_slot = EDA_hertz*time_slot
+
+
+##############
+# Heart rate #
+##############
+
+# Compute ourselves
